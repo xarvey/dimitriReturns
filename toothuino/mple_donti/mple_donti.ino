96f369ba5ecb6c7208973f2fae5f4713c73ea146
Dimitri James <james71@purdue.edu>
2016-02-01 10:36:24 -0500

    BT coding
    ;
    /\

---
+++ toothuino/mple_donti/mple_donti.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h> 
+ 
+ 
+int bluetoothTx = 6;  // TX pin of bluetooth mate (RX of arduino)
+int bluetoothRx = 7;  // RX pin of bluetooth mate (TX of arduino)
+ 
+/*
+
+NOTES
+D0 pin really shouldn't be used because the arduino programming will fail. 
+Passcode on the RN42 is 1234 by default.
+
+*/
+ 
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+
+/*----------------------SETUP----------------------------*/
+void setup() { 
+  Serial.begin(115200); //same baud rate as 
+  bluetooth.begin(115200);
+  bluetooth.println("====================");
+  bluetooth.println("Now, /that's/ SALTy"); //Will print bt name in console
+  bluetooth.println("====================");
+
+} 
+
+/*----------------------LOOP----------------------------*/
+void loop() { 
+
+  String t;
+  
+  if (bluetooth.available())
+  {
+   Serial.print((char)bluetooth.read());
+  }
+  
+  if (Serial.available())
+  {
+   bluetooth.print((char)Serial.read()); 
+  }
+  
+   bluetooth.print(t); 
+  if (t=="SALT")
+    bluetooth.print("Smart Aging and Living Technologies\n");
+  if (t=="cmdmode")
+  {
+    bluetooth.print("$");
+    bluetooth.print("$");
+    bluetooth.print("$");
+  }  
+  /*
+  if(Serial.available())
+  {
+    bluetooth.print((char)Serial.read());  
+  }
+  */
+  
+ }
