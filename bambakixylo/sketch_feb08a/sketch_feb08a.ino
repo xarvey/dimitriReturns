a9d885279831da7f03e6049d46ee301413b04fa4
Dimitri James <james71@purdue.edu>
2016-02-14 17:49:25 -0500

    Multiple read, extract only card ID

--- bambakixylo/sketch_feb08a/sketch_feb08a.ino
+++ bambakixylo/sketch_feb08a/sketch_feb08a.ino
@@ -1,92 +1,44 @@
 #include <SoftwareSerial.h>
  
-#define RXPIN 11
-#define TXPIN 10
+#define RXPIN 11 //TX on C/W connected to pin on arduino
+#define TXPIN 10 //RX on C/W connected to pin on arduino
  
-char command_scantag[]={0x18,0x03,0xFF};//const
-int incomingByte;
-SoftwareSerial myserial(RXPIN,TXPIN) ;
- 
+
+byte incomingByte;
+byte cmd_scan[] = {0x43,0x03,0x01};
+
+SoftwareSerial myserial(RXPIN,TXPIN);
+
+
 void setup()
 {
-Serial.begin(9600);
-myserial.begin(9600);
-Serial.println("YOLO");
+Serial.begin(9600); //arduino
+myserial.begin(9600); //cotton wud
+
 }
+
 void loop()
 {
-  
-  
-/* 
-myserial.write(0x33);
-myserial.write(0x0F);
-myserial.write(0x0C);
-myserial.write(0x01);
-myserial.write(0x02);
-myserial.write(0x03);
-myserial.write(0x04);
-myserial.write(0x05);
-myserial.write(0x06);
-myserial.write(0x07);
-myserial.write(0x07);
-myserial.write(0x07);
-myserial.write(0x07);
-myserial.write(0x08);
-myserial.write(0x09);
-myserial.write(0x10);
-myserial.write(0x6A);
-myserial.write(0x0F);
-*/
-  
-Serial.println("\n\n===============\n");
-//turn on antenna
-while(Serial.available())
-{
-incomingByte = Serial.read();
-if(incomingByte='r')
-{
-  
-/*
-myserial.print(0x33);
-myserial.print(0x0F);
-myserial.print(0x0C);
-myserial.print(0x01);
-myserial.print(0x02);
-myserial.print(0x03);
-myserial.print(0x04);
-myserial.print(0x05);
-myserial.print(0x06);
-myserial.print(0x07);
-myserial.print(0x07);
-myserial.print(0x07);
-myserial.print(0x07);
-myserial.print(0x08);
-myserial.print(0x09);
-myserial.print(0x10);
-myserial.print(0x6A);
-myserial.print(0x0F);
-*/
-}
- 
- 
-myserial.write(0x37);
-myserial.write(0x05);
-myserial.write(0x01);
-myserial.write(0x02);
-myserial.write(0x06);  
 
-myserial.print(0x37);
-myserial.print(0x05);
-myserial.print(0x01);
-myserial.print(0x02);
-myserial.print(0x06);  
-}
-delay (1000);
+  
+myserial.write(cmd_scan, sizeof(cmd_scan)); //read command
+
+delay(1000); //Wait 1 Sec so it doesn't go H.A.M.
+
+int counter=0;
+int res[30]={0};
 while (myserial.available())
 {
 incomingByte = myserial.read();
-Serial.println(incomingByte,HEX);
+
+if (counter>=10) //store only ID bytes returned form scan command
+  res[counter-10]=incomingByte;
+counter++;
 }
+
+for (int i=0; i<12; i++)
+  Serial.print(res[i],HEX); //
+
+Serial.println("");
 }
- 
 
